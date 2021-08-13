0bec110999f0de89286c8aaa3cec68bb0097c0cc
Segwegler <-NA->
2021-08-13 01:38:41 -0800

    changed content from test message to main-view component

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//import components
+import MainView from './components/main-view/main-view';
+
 //import statement to indicate that you need to bundle './index.scss'
 import './index.scss';
+
+
 
 //main component
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <div className="my-flix">
-        <div>Good morning</div>
-      </div>
+      <MainView />
     );
   }
 }
