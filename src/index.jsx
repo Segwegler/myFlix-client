bf8fbef702377ab8a2fecde88ee5acb6d80f8ce7
Segwegler <-NA->
2021-09-02 20:46:01 -0800

    Updated index to use redux

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import moviesApp from './reducers/reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 
 //import components
 import MainView from './components/main-view/main-view';
@@ -7,13 +11,15 @@ import MainView from './components/main-view/main-view';
 //import statement to indicate that you need to bundle './index.scss'
 import './index.scss';
 
-
+const store = createStore(moviesApp, devToolsEnhancer());
 
 //main component
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <MainView />
+      <Provider store={store}>
+        <MainView />
+      </Provider>
     );
   }
 }
