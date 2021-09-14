c5b7870029e823da85412cc27092f9dd21bc0de1
Segwegler <-NA->
2021-09-13 21:26:26 -0800

    Updated movie card to use redux to check if movie is a favorite

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,38 +2,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import { connect } from 'react-redux';
 
 import { Link } from 'react-router-dom';
 
+const mapStateToProps = state => {
+  const {user} = state;
+  return {user};
+};
 
+function MovieCard(props){
 
-export default class MovieCard extends React.Component {
-
-  render(){
-    const {movie, addMovie} = this.props;
-
-    return (
-      <Card className="h-100">
-        <Card.Img variant="top" src={movie.ImagePath} />
-        <Card.Body>
-          <Card.Title>{movie.Title}</Card.Title>
-          <Card.Text>{movie.Description.length>=97 ? movie.Description.substring(0,100) + "..." : movie.Description }</Card.Text>
-          <Link to={`/movies/${movie._id}`} >
-            <Button variant="link">Open</Button>
-          </Link>
-          <Link to={`/directors/${movie.Director.Name}`}>
-            <Button variant="link">Director</Button>
-          </Link>
-          <Link to={`/genres/${movie.Genre.Name}`}>
-            <Button variant="link">Genre</Button>
-          </Link>
-          <Button onClick={() => addMovie(movie._id)}>Add to favorites</Button>
-        </Card.Body>
-      </Card>
-    );
+  const {movie, addMovie, removeMovie, user} = props;
+  let favorite = false;
+  if(Object.keys(user).indexOf("FavoriteMovies") > -1){
+    if(user.FavoriteMovies.includes(movie._id)){
+      favorite = true;
+    }
   }
+
+  return (
+    <Card className="h-100">
+      <Card.Img variant="top" src={movie.ImagePath} />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Description.length>=97 ? movie.Description.substring(0,100) + "..." : movie.Description }</Card.Text>
+        <Link to={`/movies/${movie._id}`} >
+          <Button variant="link">Open</Button>
+        </Link>
+        <Link to={`/directors/${movie.Director.Name}`}>
+          <Button variant="link">Director</Button>
+        </Link>
+        <Link to={`/genres/${movie.Genre.Name}`}>
+          <Button variant="link">Genre</Button>
+        </Link>
+        { favorite
+         ? <Button variant="danger" onClick={() => removeMovie(movie._id)}>Remove</Button>
+         : <Button onClick={() => addMovie(movie._id)}>Add to favorites</Button>
+        }
+      </Card.Body>
+    </Card>
+  );
 }
 
+
+export default connect(mapStateToProps)(MovieCard);
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
