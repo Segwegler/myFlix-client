b646d2e11329d60a4b6c66e12afbb3d9f5271303
Segwegler <-NA->
2021-09-13 21:47:38 -0800

    Updated movies-list
    Added movies from redux state
    updated movie card to pass button functions

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import Col from 'react-bootstrap/Col';
+
+//import redux connect function
 import { connect } from 'react-redux';
 
+//import used bootstrap components
+import Col from 'react-bootstrap/Col';
+
+//import used components
 import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
 import MovieCard from '../movie-card/movie-card';
 
+//Maps used state components
 const mapStateToProps = state => {
-  const { visibilityFilter } = state;
-  return { visibilityFilter };
+  const { visibilityFilter, movies } = state;
+  return { visibilityFilter, movies };
 };
+
 
 function MoviesList(props) {
   const { movies, visibilityFilter } = props;
   let filteredMovies = movies;
-
+  //console.log(props, "movieslist");
   if(visibilityFilter !== ''){
     filteredMovies = movies.filter(m => m.Title.toLowerCase().includes(visibilityFilter.toLowerCase()));
   }
 
   if(!movies) return <div className="main-view"/>;
-
-  console.log(movies);
 
   return <>
     <Col md={12} style={{margin: '1em' }}>
@@ -28,7 +33,7 @@ function MoviesList(props) {
     </Col>
     {filteredMovies.map(m => (
       <Col md={3}  key={m._id} className="p-1">
-        <MovieCard movie={m} />
+        <MovieCard movie={m} addMovie={id => props.addMovie(id)} removeMovie={id => props.removeMovie(id)}/>
       </Col>
     ))}
   </>;
