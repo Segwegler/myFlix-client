36e4cf408b202323729c959169d980f9b194c653
Segwegler <-NA->
2021-09-13 20:50:16 -0800

    Updated to use redux

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import axios from 'axios';
 
-import { connect } from 'react-redux';
-
+//used to route to different views
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
-import { setMovies } from '../../actions/actions';
+import { connect } from 'react-redux';
 
-import MoviesList from '../movies-list/movies-list';
+//action/reducer import
+import { setMovies, setUser } from '../../actions/actions';
 
+//import used bootstrap components
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
+
+//import created components
+import MoviesList from '../movies-list/movies-list';
 import LoginView from '../login-view/login-view';
 import RegistrationView from '../registration-view/registration-view';
 import MyNavBar from '../nav-bar/nav-bar';
@@ -22,50 +26,56 @@ import DirectorView from "../director-view/director-view";
 import GenreView from "../genre-view/genre-view";
 import UserView from "../user-view/user-view";
 
+const API_ADDRESS = "https://nsegler-myflixdb.herokuapp.com";
 
-
-
-
+//The mainview controlls the view displayed
 class MainView extends React.Component {
-  constructor(){
-    super();
-    this.state ={
-      user: null
-    };
-  }
 
+  //Loads movies and user if user and token are stored in local stroage
   componentDidMount(){
     let accessToken = localStorage.getItem('token');
     if(accessToken !== null){
-      this.setState({
-        user: localStorage.getItem('user')
-      });
+      if(!("Username" in this.props.user)){
+        this.getUser();
+      }
     }
-
     this.getMovies();
   }
 
+  //retrives the movie list from the api
   getMovies() {
-    axios.get('https://nsegler-myflixdb.herokuapp.com/movies').then(response => {
+    axios.get(`${API_ADDRESS}/movies`).then(response => {
       this.props.setMovies(response.data);
     }).catch(error => {
       console.log(error);
     });
   }
 
+  login(username, password) {
+    axios.post(`${API_ADDRESS}/login`, {
+      Username: username,
+      Password: password
+    }).then( response => {
+      this.onLoggedIn(response.data);
+    }).catch( e => {
+      console.log('user not found',e);
+    });
+  }
+
   //after login processing from login-view
   onLoggedIn(authData) {
-    this.setState({
-      user: authData.user.Username
-    });
+    // this.setState({
+    //   user: authData.user.Username
+    // });
+    this.props.setUser(authData.user);
     localStorage.setItem('token', authData.token);
     localStorage.setItem('user' , authData.user.Username);
   }
 
   onUpdate(userData) {
-    this.setState({
-      user: userData.Username
-    });
+    // this.setState({
+    //   user: userData.Username
+    // });
     localStorage.setItem('user' , userData.Username);
   }
 
@@ -73,17 +83,17 @@ class MainView extends React.Component {
   onLoggedOut(){
     localStorage.removeItem('token');
     localStorage.removeItem('user');
-    this.setState({
-      user:null
-    });
+    this.props.setUser({});
+    window.open("/","_self");
   }
 
 
   getUser() {
-    axios.get(`https://nsegler-myflixdb.herokuapp.com/users/${localStorage.getItem('user')}`, {
+    axios.get(`${API_ADDRESS}/users/${localStorage.getItem('user')}`, {
       headers: { Authorization: `Bearer ${localStorage.getItem('token')}`}
     }).then(response => {
-      console.log(response);
+      //console.log(response);
+      this.props.setUser(response.data);
       return response.data;
     }).catch(function (error) {
       console.log(error);
@@ -95,30 +105,42 @@ class MainView extends React.Component {
       window.open("/login","_self");
       return ;
     }
-    axios.post(`https://nsegler-myflixdb.herokuapp.com/users/${localStorage.getItem('user')}/movies/${id}`,{},
+    if(this.props.user.FavoriteMovies.includes(id)){
+      return;
+    }
+    axios.post(`${API_ADDRESS}/users/${localStorage.getItem('user')}/movies/${id}`,{},
       {headers: { Authorization: `Bearer ${localStorage.getItem('token')}`}
     }).then(response =>{
-        console.log(response);
+        this.getUser();
     }).catch(function (error){
       console.log(error);
     });
   }
 
   removeMovie(id){
-    axios.delete(`https://nsegler-myflixdb.herokuapp.com/users/${localStorage.getItem('user')}/movies/${id}`,
+    if(!localStorage.getItem('user')){
+      window.open("/login","_self");
+      return ;
+    }
+    if(!this.props.user.FavoriteMovies.includes(id)){
+      return;
+    }
+    axios.delete(`${API_ADDRESS}/users/${localStorage.getItem('user')}/movies/${id}`,
       {headers: { Authorization: `Bearer ${localStorage.getItem('token')}`}
     }).then(response =>{
-        console.log(response);
+        this.getUser();
     }).catch(function (error){
       console.log(error);
     });
   }
 
   render(){
-    let { user } = this.state;
-    let { movies } = this.props;
 
-    const nav = <MyNavBar user={user} onLoggedOut = {() => this.onLoggedOut()} />
+    let { movies, user } = this.props;
+
+    //console.log(user, "main view render");
+
+    const nav = <MyNavBar onLoggedOut = {() => this.onLoggedOut()} />
 
     return (
       <Router>
@@ -128,20 +150,19 @@ class MainView extends React.Component {
             <Route exact path="/" render={() => {
 
               if (movies.length === 0) return <div className="main-view" />;
-
-              return <MoviesList movies={movies}/>;
+              return <MoviesList addMovie={id => this.addMovie(id)} removeMovie={id => this.removeMovie(id)}/>;
 
             }} />
 
             <Route path="/login" render={({history}) => {
-              if(user) return <Redirect to="/" />
+              if("Username" in user) return <Redirect to="/" />
               return <Col md={8}>
-                <LoginView onLoggedIn={user => this.onLoggedIn(user)} onBackClick={() => history.goBack()}/>
+                <LoginView onLoggedIn={user => this.onLoggedIn(user)} login={(username, password) => this.login(username, password)} onBackClick={() => history.goBack()}/>
               </Col>
             }} />
 
             <Route path="/register" render={({history}) => {
-              if(user) return <Redirect to="/" />
+              if("Username" in user) return <Redirect to="/" />
               return <Col md={8}>
                 <RegistrationView onBackClick={() => history.goBack()}/>
               </Col>
@@ -150,13 +171,13 @@ class MainView extends React.Component {
             <Route path="/user" render={({history}) => {
               if(!user) return <Redirect to="/login" />
               return <Col md={10}>
-                <UserView movies={movies} onLoggedOut={() => this.onLoggedOut()}removeMovie={id => this.removeMovie(id)} onUpdate={userData => this.onUpdate(userData)} onBackClick={() => history.goBack()}/>
+                <UserView movies={movies} onLoggedOut={() => this.onLoggedOut()} removeMovie={id => this.removeMovie(id)} onUpdate={userData => this.onUpdate(userData)} onBackClick={() => history.goBack()}/>
               </Col>
             }} />
 
             <Route path="/movies/:movieId" render={({ match, history }) => {
               return <Col md={8}>
-                <MovieView movie={movies.find(m => m._id === match.params.movieId)} onBackClick={() => history.goBack()} />
+                <MovieView movie={movies.find(m => m._id === match.params.movieId)} addMovie={id => this.addMovie(id)} removeMovie={id => this.removeMovie(id)} onBackClick={() => history.goBack()} />
               </Col>
             }} />
 
@@ -182,7 +203,10 @@ class MainView extends React.Component {
 }
 
 let mapStateToProps = state => {
-  return { movies: state.movies}
+  return {
+    movies: state.movies,
+    user: state.user
+  }
 }
 
-export default connect(mapStateToProps, { setMovies } )(MainView);
+export default connect(mapStateToProps, { setMovies, setUser } )(MainView);
